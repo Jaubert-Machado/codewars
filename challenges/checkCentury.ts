040a71cc22d7a0b60d073162f3348cfdaeff7722
Jaubert Machado <jaubertmv@outlook.com>
2023-04-08 16:32:05 -0300

    check century exercise

---
+++ challenges/checkCentury.ts
@@ -0,0 +1,7 @@
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+export default function checkCentury(year: number): number {
+  return Math.ceil(year / 100);
+}
+
+// Pontos a melhorar : Essa solução considera que qualquer numero decimal seja arredondado para cima, sendo assim, 2.01 será arredondando para 3, ou seja, ano 201 é arredondado para 3.
--- playground.ts
+++ playground.ts
@@ -1,7 +1,5 @@
-const str = "8 j 8   mBliB8g  imjB8B8  jl  B";
-
-function noSpace(str: string): string {
-  return str.replaceAll(" ", "");
+function century(year: number) {
+  return Math.ceil(year / 100);
 }
 
-console.log(noSpace(str));
+console.log(century(2002));
